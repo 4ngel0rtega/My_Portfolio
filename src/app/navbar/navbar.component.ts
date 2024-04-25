b9e42faada34587328cc4c448944d358db5f9516
4ngel0rtega <ang3l.ortega21@gmail.com>
2024-04-25 11:36:03 -0600

    Add: Styles are added.

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, ElementRef, ViewChild, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
@@ -7,6 +7,21 @@ import { Component } from '@angular/core';
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
-export class NavbarComponent {
 
+export class NavbarComponent implements AfterViewInit {
+  @ViewChild('navbarScroll') navbarScroll!: ElementRef;
+
+  ngAfterViewInit() {
+    this.onWindowScroll();
+  }
+
+  @HostListener('window:scroll') onWindowScroll() {
+    const element = this.navbarScroll.nativeElement;
+    const scrolled = window.scrollY > 50;
+    if (scrolled) {
+      element.classList.add('scrolled');
+    } else {
+      element.classList.remove('scrolled');
+    }
+  }
 }
